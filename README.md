1062e6bf7b4a7d4648c3ae250f86514682fb09f4
Brandon Alves <brandonalvesn@gmail.com>
2021-07-31 19:29:28 -0300

    função de sorteio finalizada

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PaginaDeSorteio
+
+Pagina de sorteio: Digite um numero maximo e sera mostrado um numero aleatorio.
+JavaScript, CSS 
--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 'use strict'
 
 let numberMax = document.getElementById("np");
-let show = document.querySelector(".show");
+let show = document.querySelector(".show h1");
 const draw = document.getElementById("draw");
 
 function start(){
     let value = numberMax.value;
    let sorting = Math.round(Math.random() * value);
 
+  if(document.getElementById("np").value == 0){
+      alert("Digite um numero diferente!")
+     return show.innerHTML = "?";
+  }
+
+   if(sorting == 0){
+        sorting++
+        show.innerHTML = sorting;
+   }
+
    show.innerHTML = sorting;
-
-
 };
